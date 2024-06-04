5da0b8efcf9b40b3a485940e99009ca004f4ac43
Amelia Lawrence <55457044+ahlawrence@users.noreply.github.com>
2024-06-04 14:49:10 -0400

    Update execute_python.sh

--- execute_python.sh
+++ execute_python.sh
@@ -1,4 +1,4 @@
 printf "fasta\tline_1\tline_2\tfourfold_sites\tfourfold_diff\n" > fourfold.txt
 for i in *.fa; do 
-	python fourfold_anynumber.py $i >> fourfold.txt
+	python fourfold_samelength_any.py $i >> fourfold.txt
 done
